ba7b44041a0e68366d81ff942ac1e188ab25dfe2
DIR\clifford.k.roberson <cliffroberson@gmail.com>
2018-05-18 10:40:28 -0500

    buggy version of readingCSV file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,29 @@ namespace AIHackathon
     {
         static void Main(string[] args)
         {
+
+            List<string> listA = new List<string>();
+            List<string> listB = new List<string>();
+            using (var reader = new StreamReader(@"dataservices_ubermedia_raw.csv"))
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+
+            for (int i=0;i<listA.Count;i++)
+            {
+                Console.WriteLine(listA[0]);
+            }
             
+
+
             Console.WriteLine("Say something");
 
             string response = Console.ReadLine();
