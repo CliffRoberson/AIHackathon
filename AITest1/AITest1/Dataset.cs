b0848946b6119b5902e7f477ccad363e68571ab4
Goggans, Shane <shane.goggans@accenture.com>
2018-05-18 10:36:55 -0500

    Added new console AI project

---
+++ AITest1/AITest1/Dataset.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AITest1
+{
+    public class Dataset
+    {
+
+
+
+        public static void Open(string filePath)
+        {
+            Console.WriteLine($"Loading dataset {filePath}");
+
+            string[] dataLines = System.IO.File.ReadAllLines(filePath);
+
+            foreach (var line in dataLines)
+            {
+                string[] rows = line.Split(',');
+            }
+
+            Console.WriteLine($"Done loading dataset {filePath}");
+        }
+    }
+}
---
+++ AITest1/AITest1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AITest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Loading datasets...");
+
+            //
+
+            Console.WriteLine("Done loading datasets!");
+        }
+    }
+}
