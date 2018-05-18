6867707df1a45b3b7d3c6f603716414e50b2527c
Goggans, Shane <shane.goggans@accenture.com>
2018-05-18 11:24:56 -0500

    Added dataset classes

--- AITest1/AITest1/Dataset.cs
+++ AITest1/AITest1/Dataset.cs
@@ -6,21 +6,42 @@ namespace AITest1
 {
     public class Dataset
     {
+        private Dictionary<string, int> Headers;
+        private string[][] Values;
 
+        public int Count { get; set; }
 
+        public Dataset(string filePath, Dictionary<string, int> headers)
+        {
+            Open(filePath, headers);
+        }
 
-        public static void Open(string filePath)
+        public void Open(string filePath, Dictionary<string, int> headers)
         {
             Console.WriteLine($"Loading dataset {filePath}");
 
+            Headers = headers;
             string[] dataLines = System.IO.File.ReadAllLines(filePath);
+            Array.Resize(ref Values, dataLines.Length);
+            Count = dataLines.Length;
 
-            foreach (var line in dataLines)
+            for (int i = 0; i < dataLines.Length; i++)
             {
-                string[] rows = line.Split(',');
+                string[] rows = dataLines[i].Split(',');
+                Array.Resize(ref Values[i], rows.Length);
+
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    Values[i][j] = rows[j];
+                }
             }
 
             Console.WriteLine($"Done loading dataset {filePath}");
         }
-    }
+
+        public string GetValue(string headerName, int index)
+        {
+            return Values[index][Headers[headerName]];
+        }
+    } // EOC
 }
--- AITest1/AITest1/Program.cs
+++ AITest1/AITest1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AITest1
 {
@@ -8,7 +9,21 @@ namespace AITest1
         {
             Console.WriteLine("Loading datasets...");
 
-            //
+            // dataservices_ubermedia_raw.consumer_id	dataservices_ubermedia_raw.visit_date	dataservices_ubermedia_raw.visit_time	dataservices_ubermedia_raw.visit_day_of_week	dataservices_ubermedia_raw.visit_timezone	dataservices_ubermedia_raw.visit_timestamp	dataservices_ubermedia_raw.polygon_name_new	dataservices_ubermedia_raw.polygon_category				
+            var d1Headers = new Dictionary<string, int>();
+            d1Headers.Add("ConsumerId", 0);
+            d1Headers.Add("VisitDate", 1);
+            d1Headers.Add("VisitTime", 2);
+            d1Headers.Add("VisitDayOfWeek", 3);
+            d1Headers.Add("VisitTimeZone", 4);
+            d1Headers.Add("VisitTimestamp", 5);
+            d1Headers.Add("PolygonCategory", 6);
+            var d1 = new Dataset(@"C:\Users\shane.goggans\Desktop\Datasets\dataservices_ubermedia_raw.csv", d1Headers);
+
+            for (int i = 0; i < d1.Count; i++)
+            {
+                Console.WriteLine(d1.GetValue("ConsumerId", i));
+            }
 
             Console.WriteLine("Done loading datasets!");
         }
